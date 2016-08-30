a81fad4499dcc9eda92e97d489ddd7c4a8a14583
m47jiang <michelyjiang@hotmail.com>
2016-08-29 20:09:04 -0400

    get scoreboard added

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -15,3 +15,19 @@ function postScore(username, score) {
         console.log(response);
     });
 }
+
+function getScore {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://aqueduct-tycoon.firebaseio.com/.json?auth=zz9yZ5dPm9f1y4oHud1DBhIFDi4P0JFkSSHgnoic",
+        "method": "GET",
+        "headers": {
+        "cache-control": "no-cache",
+    }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+}
